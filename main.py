591884b40bcd6191b41177d7a7522d6bc1ca7f99
Marica <bottegamarica02@gmail.com>
2024-03-04 17:54:50 +0100

    create screen

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+# scene variables
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WINDOW = SCREEN_HEIGHT * SCREEN_WIDTH
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
