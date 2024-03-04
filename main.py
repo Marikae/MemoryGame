db4d2cbca508b22ea210220a613122aa26bffea3
Marica <bottegamarica02@gmail.com>
2024-03-04 18:01:08 +0100

    backgound insert

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ SCREEN_HEIGHT = 600
 WINDOW = SCREEN_HEIGHT * SCREEN_WIDTH
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+background = pygame.image.load("img\\background.png")  #immagine di sfondo
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 run = True
 
@@ -15,3 +17,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    screen.blit(background, (0, 0)) #color the screen
+    pygame.display.update() #aggiorna la finestra di gioco or pygame.display.flip()
+
+pygame.quit() #chiude programma
